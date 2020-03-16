17793124a978693dcb61a43e2b66c6ee5a3bf55c
shivashish123 <shivashishsuman.23@gmail.com>
2020-03-16 18:01:15 +0530

    Express Session Part 1

--- Server-side Development/Week-3/conFusionServer/app.js
+++ Server-side Development/Week-3/conFusionServer/app.js
@@ -9,6 +9,8 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 const mongoose = require('mongoose');
 const url = 'mongodb://localhost:27017/conFusion';
@@ -27,13 +29,22 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+//app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+	name: 'session-id',
+	secret: '12345-67890-09876-54321',
+  	saveUninitialized: false,
+  	resave: false,
+  	store: new FileStore()
+}));
+
 
 function auth(req, res, next) {
 
-	console.log(req.signedCookies);
+	console.log(req.session);
 
-	if (!req.signedCookies.user) {
+	if (!req.session.user) {
 
 		var authHeader = req.headers.authorization;
 
@@ -51,7 +62,7 @@ function auth(req, res, next) {
 
 		if (username == 'admin' && password == 'password') 
 		{
-			res.cookie('user','admin', { signed : true});
+			req.session.user = 'admin';
 			next(); // authorized
 		}
 		else 
@@ -63,7 +74,7 @@ function auth(req, res, next) {
 		}
 	}
 	else {
-		if (req.signedCookies.user === 'admin') {
+		if (req.session.user === 'admin') {
 			next();
 		}
 		else {

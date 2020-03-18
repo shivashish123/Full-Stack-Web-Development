7383a69ba1624374d27920faa86a2e4de33ca407
shivashish123 <shivashishsuman.23@gmail.com>
2020-03-18 11:17:47 +0530

    File Upload

--- Server-side Development/Week-4/conFusionServer/app.js
+++ Server-side Development/Week-4/conFusionServer/app.js
@@ -8,6 +8,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 var config = require('./config');
 
 var session = require('express-session');
@@ -50,6 +51,7 @@ app.use('/users', usersRouter);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/imageUpload',uploadRouter);
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
